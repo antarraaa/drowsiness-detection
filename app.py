a26429de336c114c4d019d7f31f74ea525b4ba74
antarraaa <sarafantara@gmail.com>
2026-06-21 01:30:26 +0530

    updated code

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     print("Starting Drowsiness Detection System...")
 
     try:
-        run_detector()
+        run()
     except Exception as e:
         print("Error:", e)
